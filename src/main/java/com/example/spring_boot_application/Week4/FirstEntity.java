16c3c8c65afa63fde08c00fbf48cd18b99f52471
Muhammad <malfa08147558757@gmail.com>
2026-02-13 07:22:37 +0100

    Week4 submission

---
+++ src/main/java/com/example/spring_boot_application/Week4/FirstEntity.java
@@ -0,0 +1,18 @@
+package com.example.spring_boot_application.Week4;
+
+import java.time.LocalDate;
+
+@Entity
+public class FirstEntity {
+    private String firstname;
+    private String middlename;
+    private String lastname;
+    private String email;
+    private String phoneNumber;
+    private String country;
+    private String stateOfOrigin;
+    private LocalDate dateOfBirth;
+    private String username;
+    private String password;
+    private String confirmPassword;
+}
